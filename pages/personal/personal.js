6b6d797bcc479e53620fb08a8553d7b5a6f39549
lichengcai <lichengcai@koolearn.com>
2017-12-12 18:46:26 +0800

    test

---
+++ pages/personal/personal.js
@@ -0,0 +1,15 @@
+var helloData = {
+  name: 'WeChat',
+  age:12
+}
+
+// Register a Page.
+Page({
+  data: helloData,
+  changeName: function(e) {
+    // sent data change to view
+    this.setData({
+      name: 'MINA'
+    })
+  }
+})
